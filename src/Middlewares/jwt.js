b87c0e67cc385454d13401f0b3f964f8bc2f24d4
Joel Riba <joel.irungu00@gmail.com>
2025-12-02 20:03:10 +0300

    with jwt

--- src/Middlewares/jwt.js
+++ src/Middlewares/jwt.js
@@ -1,16 +1,19 @@
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    
-    if (token == null) return res.sendStatus(401); 
-    
+    if (token == null) {
+        return res.redirect('https://device-tracking-app-frontend-xi.vercel.app/about');  
+    }
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403); 
+        if (err) {
+            return res.redirect('https://device-tracking-app-frontend-xi.vercel.app/about');  
+        }
         req.user = user;
-        next(); 
+        next();  
     });
-    };
+};
 
 module.exports = authenticateToken;
