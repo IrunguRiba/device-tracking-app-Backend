1441d7bf8ec012c095f17a6910e5bf48eb875f47
Joel Riba <joel.irungu00@gmail.com>
2026-01-18 14:27:13 +0300

    Add CI

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -2,14 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../Middlewares/jwt");
 
-const { createUser, logInUser ,  adminRegister, getUserByEmailForValidation, adminLogIn, getUsers,getUserById, deleteUserById,  getUserByPin, trackingDevicesByPin} = require("../Controllers/userController");
+const { createUser, logInUser ,  adminRegister,  adminLogIn, getUsers,getUserById, deleteUserById,  getUserByPin, trackingDevicesByPin} = require("../Controllers/userController");
  
 
 router.post("/newUser",  createUser);
 router.post("/userLogIn", logInUser);
 router.post("/newAdmin", adminRegister)
 router.post ("/adminLogIn", adminLogIn)
-router.post('/userByEmail', getUserByEmailForValidation)
 router.get("/getUsers", authenticateToken, getUsers);
 router.get('/getUser/:_id', authenticateToken,  getUserById)
 router.get('/getUserByPin/:pin', authenticateToken,   getUserByPin);
